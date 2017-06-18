18deb76691b5c824ea30cc53da1283b1361d40ef
Joe Fleming <joe.fleming@gmail.com>
2017-06-18 12:57:02 -0700

    add buefy

--- src/index.js
+++ src/index.js
@@ -1,7 +1,12 @@
 /* eslint no-new:0 import/no-extraneous-dependencies:0 */
 
 import Vue from 'vue';
+import Buefy from 'buefy';
 import App from './components/App.vue';
+
+Vue.use(Buefy, {
+  defaultIconPack: 'fa',
+});
 
 new Vue({
   el: '#app',
