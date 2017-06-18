539895d0d81a2fb072b80ab12a9eb6e2efb0b606
Joe Fleming <joe.fleming@gmail.com>
2017-06-18 13:00:02 -0700

    configure buefy and add demo page

--- src/components/App.vue
+++ src/components/App.vue
@@ -1,16 +1,58 @@
 <template>
   <div id="app">
-    <h1>Welcome to your Vue.js app!</h1>
+    <top-navigation />
+    <section class="section">
+      <create-dashboard />
+    </section>
   </div>
 </template>
 
 <script>
+  import TopNavigation from './TopNavigation.vue';
+  import IndexPrompt from './IndexPrompt.vue';
+  import CreateDashboard from './CreateDashboard.vue';
+
   export default {
     name: 'app',
+    components: {
+      TopNavigation,
+      IndexPrompt,
+      CreateDashboard,
+    },
+    data: () => ({
+      checkbox: true,
+      isSwitched: true,
+    }),
   };
 </script>
 
 <!-- Global CSS -->
+<style lang="sass">
+  // Import Bulma's core
+  @import "~bulma/sass/utilities/_all";
+
+  // custom colors
+  $primary: #D31996;
+  $primary-invert: findColorInvert($primary);
+  $secondary: #0CC19A;
+  $secondary-invert: findColorInvert($secondary);
+  $info: #144580;
+  $black: #383434;
+  $light: #F0F0F0;
+  $link: $primary;
+  $link-visited: $secondary;
+
+  // Setup $colors to use as bulma classes (e.g. 'is-twitter')
+  $colors: (white: ($white, $black), black: ($black, $white), light: ($light, $light-invert), dark: ($dark, $dark-invert), primary: ($primary, $primary-invert), secondary: ($secondary, $secondary-invert), info: ($info, $info-invert), success: ($success, $success-invert), warning: ($warning, $warning-invert), danger: ($danger, $danger-invert));
+
+  // Import Bulma and Buefy styles
+  @import "~bulma";
+  @import "~buefy/src/scss/buefy";
+
+  // Import font-awesome
+  @import '~font-awesome/css/font-awesome.css';
+</style>
+
 <style>
   /* system fonts, medium/wordpress stack - https://css-tricks.com/snippets/css/system-font-stack */
   body {
---
+++ src/components/BuefyDemo.vue
@@ -0,0 +1,104 @@
+<template>
+  <div id="app">
+    <section class="container">
+      <h1 class="title">Buefy Customization Basic Setup</h1>
+
+      <div class="block">
+        <b-checkbox>Basic</b-checkbox>
+        <b-checkbox v-model="checkbox">{{ checkbox }}</b-checkbox>
+        <b-checkbox disabled>Disabled</b-checkbox>
+        <b-checkbox disabled checked>Disabled Checked</b-checkbox>
+      </div>
+
+      <div class="block">
+        <b-switch type="is-secondary">Default</b-switch>
+        <b-switch type="is-secondary" v-model="isSwitched">{{ isSwitched }}</b-switch>
+        <b-switch type="is-secondary" disabled>Disabled</b-switch>
+        <b-switch type="is-secondary" disabled checked>Disabled Checked</b-switch>
+      </div>
+
+      <b-field label="Name">
+        <b-input placeholder="John Silver"></b-input>
+      </b-field>
+
+      <b-field label="Email"
+          type="is-danger"
+          message="This email is invalid">
+          <b-input type="email"
+              value="john@"
+              maxlength="30">
+          </b-input>
+      </b-field>
+
+      <b-field label="Username"
+          type="is-success"
+          message="This username is available">
+          <b-input value="johnsilver" maxlength="30"></b-input>
+      </b-field>
+
+      <div class="block">
+          <b-icon icon="heartbeat" type="is-primary"></b-icon>
+          <b-icon icon="heartbeat" type="is-secondary" invert></b-icon>
+          <b-icon icon="user" type="is-info"></b-icon>
+          <b-icon icon="user" type="is-warning"></b-icon>
+          <b-icon icon="user" type="is-danger"></b-icon>
+          <b-icon icon="user" type="is-success"></b-icon>
+      </div>
+
+      <div class="block">
+        <b-icon icon="users" type="is-white"></b-icon>
+        <b-icon icon="users" type="is-black"></b-icon>
+        <b-icon icon="users" type="is-light"></b-icon>
+        <b-icon icon="users" type="is-dark"></b-icon>
+      </div>
+
+      <b-message title="Info with icon" type="is-info" has-icon>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce id fermentum quam. Proin sagittis, nibh id hendrerit imperdiet, elit sapien laoreet elit
+      </b-message>
+
+      <b-message title="Danger with icon" type="is-danger" has-icon>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce id fermentum quam. Proin sagittis, nibh id hendrerit imperdiet, elit sapien laoreet elit
+      </b-message>
+
+      <b-message title="Primary Message" type="is-primary">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce id fermentum quam. Proin sagittis, nibh id hendrerit imperdiet, elit sapien laoreet elit
+      </b-message>
+
+      <b-message title="Secondary Message" type="is-secondary">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce id fermentum quam. Proin sagittis, nibh id hendrerit imperdiet, elit sapien laoreet elit
+      </b-message>
+
+      <b-field>
+        <b-select placeholder="select filter" expanded>
+          <option value="open_issues">Open Issues and Pull Requests</option>
+          <option value="your_issues">Your Issues</option>
+          <option value="pull_requests">Your Pull Requests</option>
+          <option value="everything">Everything</option>
+        </b-select>
+
+        <b-input icon="search" type="search" placeholder="Search..."></b-input>
+      </b-field>
+
+      <b-field>
+        <b-input placeholder="Loading..." disabled loading></b-input>
+      </b-field>
+
+    </section>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'buefy-demo',
+    data: () => ({
+      checkbox: true,
+      isSwitched: true,
+    }),
+  };
+</script>
+
+<style scoped>
+  section {
+    padding-bottom: 200px;
+  }
+</style>
