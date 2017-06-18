6ceb170f11566f1b94171579947c2f2749fedfb7
Joe Fleming <joe.fleming@gmail.com>
2017-06-18 10:54:46 -0700

    initial commit

---
+++ src/components/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="app">
+    <h1>Welcome to your Vue.js app!</h1>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'app'
+  }
+</script>
+
+<!-- Global CSS -->
+<style>
+  /* system fonts, medium/wordpress stack - https://css-tricks.com/snippets/css/system-font-stack */
+  body {
+    font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Oxygen-Sans, Ubuntu, Cantarell, "Helvetica Neue", sans-serif;
+  }
+
+  code {
+    font-family: Menlo, Monaco, Lucida Console, Liberation Mono, DejaVu Sans Mono, Bitstream Vera Sans Mono, Courier New, monospace, serif;
+    font-size: 0.9em;
+    white-space: pre-wrap;
+    color: #2c3e50;
+  }
+
+  code::before, code::after {
+    content: '`';
+  }
+</style>
