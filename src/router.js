6a4041a23149e207c66ade423ef2ecba29dfbe5c
Joe Fleming <joe.fleming@gmail.com>
2017-06-26 18:13:48 -0700

    set up routes

--- src/router.js
+++ src/router.js
@@ -1,9 +1,29 @@
 import VueRouter from 'vue-router';
 
+import BuefyDemo from './components/BuefyDemo.vue';
+import IndexPrompt from './components/IndexPrompt.vue';
+import CreateDashboard from './components/CreateDashboard.vue';
+import Dashboard from './components/Dashboard.vue';
+
 const router = new VueRouter({
   mode: 'hash',
-  routes: [
-  ],
+  routes: [{
+    name: 'index',
+    path: '/',
+    component: BuefyDemo,
+  }, {
+    name: 'load',
+    path: '/view',
+    component: IndexPrompt,
+  }, {
+    name: 'create',
+    path: '/create',
+    component: CreateDashboard,
+  }, {
+    name: 'view',
+    path: '/view/:id',
+    component: Dashboard,
+  }],
 });
 
 export default router;
