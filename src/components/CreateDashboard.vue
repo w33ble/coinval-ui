31a20da24adf8a5c6061969f6727eb64aa1f31c6
Joe Fleming <joe.fleming@gmail.com>
2017-07-05 18:15:04 -0700

    create dashboard in firebase

--- src/components/CreateDashboard.vue
+++ src/components/CreateDashboard.vue
@@ -15,10 +15,10 @@
                 <b-input placeholder="My Ducats" v-model="title"></b-input>
               </b-field>
             </div>
-            <div class="block">
+            <div class="block" v-if="allowPrivate">
               <b-switch v-model="isPrivate">Private Dashboard</b-switch>
             </div>
-            <div class="block">
+            <div class="block" v-if="allowPrivate">
               <b-field
                 label="Enter PIN"
                 v-if="isPrivate" :type="errors.pin ? 'is-danger' : ''"
@@ -28,7 +28,7 @@
               </b-field>
             </div>
             <div class="block">
-              <button class="button is-primary">Create</button>
+              <button class="button is-primary" :class="{ 'is-loading': loading }">Create</button>
             </div>
           </form>
         </section>
@@ -39,30 +39,59 @@
 </template>
 
 <script>
-export default {
-  name: 'create-dashboard',
-  data: () => ({
-    title: '',
-    isPrivate: false,
-    pin: '',
-    errors: {
-      title: null,
-      pin: null,
-    },
-  }),
-  methods: {
-    create() {
-      this.errors.title = null;
-      this.errors.pin = null;
+  import { ref } from '../lib/firebase';
+  import guid from '../lib/guid';
 
-      if (this.title.length === 0) this.errors.title = 'You must provide a name for the dashboard';
-      if (this.isPrivate && this.pin.length !== 4) this.errors.pin = 'You must provide a 4 digit PIN';
-      if (this.errors.title || this.errors.pin) return;
+  export default {
+    name: 'create-dashboard',
+    data: () => ({
+      title: '',
+      isPrivate: false,
+      allowPrivate: false,
+      pin: '',
+      loading: false,
+      errors: {
+        title: null,
+        pin: null,
+      },
+    }),
+    methods: {
+      create() {
+        this.errors.title = null;
+        this.errors.pin = null;
 
-      // TODO: create dashboard
-      console.log(`Create: ${this.title}`);
-      if (this.isPrivate) console.log(`With PIN: ${this.pin}`);
+        if (this.title.length === 0) this.errors.title = 'You must provide a name for the dashboard';
+        if (this.isPrivate && this.pin.length !== 4) this.errors.pin = 'You must provide a 4 digit PIN';
+        if (this.errors.title || this.errors.pin) return;
+
+        this.loading = true;
+
+        function createNewDashboard() {
+          const id = guid();
+          const dbRef = ref(`/dashboards/${id}`);
+
+          return dbRef.once('value').then((snap) => {
+            if (!snap.val() || snap.val().coins.length === 0) return [id, dbRef];
+            return createNewDashboard();
+          });
+        }
+
+        createNewDashboard()
+        .then(([id, dbRef]) => {
+          dbRef.set({
+            title: this.title,
+            isPrivate: false,
+            timestamp: (new Date()).getTime(),
+            holdings: [],
+          })
+          .then(() => {
+            this.$router.push({
+              name: 'view',
+              params: { id },
+            });
+          });
+        });
+      },
     },
-  },
-};
+  };
 </script>
