2468ef14131ac209ca6e612caa986d1c6fd00a32
Joe Fleming <joe.fleming@gmail.com>
2017-07-06 17:06:54 -0700

    add some controls

--- src/components/Dashboard.vue
+++ src/components/Dashboard.vue
@@ -2,6 +2,20 @@
   <section class="section">
     <div class="columns">
       <div class="column is-8-widescreen is-offset-2-widescreen">
+        <div style="margin-bottom: 10px; text-align: right;">
+          <!-- action buttons at the top -->
+          <button class="button" @click="fetchPrices">
+            <b-icon icon="refresh" :class="{'fa-spin': refreshingPrice}"></b-icon>
+            <span>Refresh</span>
+          </button>
+
+          <button class="button" @click="doAdd">
+            <b-icon icon="plus"></b-icon>
+            <span>Add</span>
+          </button>
+        </div>
+
+        <!-- all the holdings as cards -->
         <div class="columns is-multiline" v-if="!loading">
           <div class="column is-half" v-for="holding in holdings">
             <dashboard-card :holding="holding" @edit="doEdit" @delete="doDelete"></dashboard-card>
@@ -9,13 +23,18 @@
 
         </div>
 
+        <!-- loading message -->
         <div v-if="loading">
           Loading...
         </div>
 
+        <!-- no holdings message -->
         <div v-if="!loading && (!holdings || holdings.length < 1)">
           No holdings to show. You should add some!
         </div>
+
+        <!-- late updated at message -->
+        <div v-if="lastUpdated">Last update: {{ lastUpdated }}</div>
       </div>
     </div>
   </section>
@@ -36,7 +55,7 @@
           source: ref(`dashboards/${dashboardId}`),
           readyCallback: (dashboard) => {
             if (!dashboard.val()) {
-              this.$snackbar.open({
+              this.$toast.open({
                 message: 'Invalid dashboard',
                 type: 'is-danger',
                 position: 'is-top',
@@ -48,20 +67,34 @@
             }
           },
         },
-        holdings: {
-          source: ref(`holdings/${dashboardId}`),
-        },
+        holdings: ref(`holdings/${dashboardId}`),
       };
     },
     data: () => ({
       loading: true,
+      refreshingPrice: false,
+      lastUpdated: null,
     }),
     methods: {
+      fetchPrices() {
+        const unique = (value, index, self) => self.indexOf(value) === index;
+        const types = this.holdings.map(h => h.cointype).filter(unique);
+
+        this.refreshingPrice = true;
+        setTimeout(() => {
+          this.lastUpdated = (new Date()).toString();
+          this.refreshingPrice = false;
+          this.$toast.open(`Refresh prices: ${types.join(', ')}`);
+        }, 300);
+      },
+      doAdd() {
+        this.$toast.open('Create holding');
+      },
       doEdit(holding) {
-        this.$snackbar.open(`Edit ${holding.id}`);
+        this.$toast.open(`Edit ${holding.id}`);
       },
       doDelete(holding) {
-        this.$snackbar.open(`Delete ${holding.id}`);
+        this.$toast.open(`Delete ${holding.id}`);
       },
     },
   };
