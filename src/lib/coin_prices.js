c0c5cf85cfd281ee978f7fbdb980833efb3aba9d
Joe Fleming <joe.fleming@gmail.com>
2017-07-17 18:40:11 -0700

    add coin prices module

---
+++ src/lib/coin_prices.js
@@ -0,0 +1,20 @@
+import axios from 'axios';
+
+const client = axios.create({
+  baseURL: 'https://us-central1-coinval-e60f1.cloudfunctions.net',
+  timeout: 5000,
+  headers: { 'X-Request-Source': 'coinval' },
+});
+
+export default function (currency = 'usd', exchange = 'poloniex') {
+  return client.get('/prices')
+  .then(res => res.data)
+  .then(data => ({
+    btc: data[`${exchange}:btc${currency}`],
+    ltc: data[`${exchange}:ltc${currency}`],
+    eth: data[`${exchange}:eth${currency}`],
+    xmr: data[`${exchange}:xmr${currency}`],
+    dash: data[`${exchange}:dash${currency}`],
+    xrp: data[`${exchange}:xrp${currency}`],
+  }));
+}
