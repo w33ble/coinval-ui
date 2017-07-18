2280d17d0edefafb40ff164c1e5fdb6ba4e710f3
Joe Fleming <joe.fleming@gmail.com>
2017-07-17 18:28:39 -0700

    add response caching

--- functions/index.js
+++ functions/index.js
@@ -1,12 +1,36 @@
+/* eslint no-console:0 */
 const functions = require('firebase-functions');
 const admin = require('firebase-admin');
 const Cryptowatch = require('cryptowatch');
 
+const cw = new Cryptowatch();
+const cache = {
+  lastCheckTime: 0,
+  prices: {},
+};
+
 // enable communication with firebase db
 admin.initializeApp(functions.config().firebase);
-const cw = new Cryptowatch();
 
 // create prices endpoint
 exports.prices = functions.https.onRequest((request, response) => {
-  cw.prices().then(res => response.send(res));
+  const curTime = (new Date()).getTime();
+  if (curTime - cache.lastCheckTime >= 30000) {
+    console.log('cache skipped');
+
+    // update the last check time
+    cache.lastCheckTime = curTime;
+
+    // fetch price info from api
+    cw.prices().then((res) => {
+      // cache the response
+      cache.prices = res;
+      response.send(res);
+    });
+  } else {
+    console.log('cache hit!');
+
+    // send the cached value
+    response.send(cache.prices);
+  }
 });
